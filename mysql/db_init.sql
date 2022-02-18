82f4607d54d76b4533beb681dab3ef9e1e0231bd
David W de Jesus <davidwj15@gmail.com>
2022-02-18 19:52:19 -0300

    primeiro commit

---
+++ mysql/db_init.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+);
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+);
+
+DROP TABLE IF EXISTS `product_tag`;
+CREATE TABLE `product_tag` (
+   `product_id` int NOT NULL,
+   `tag_id` int NOT NULL,
+   PRIMARY KEY (`product_id`,`tag_id`),
+   CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+   CONSTRAINT `tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`)
+);
